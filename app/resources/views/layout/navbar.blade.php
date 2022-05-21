6849873b3b5acda95abba9f50bfc5561fae8d1bc
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-21 11:29:38 +0630

    Change ui'

--- app/resources/views/layout/navbar.blade.php
+++ app/resources/views/layout/navbar.blade.php
@@ -1,7 +1,9 @@
 {{--        Navbar Start--}}
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg" style="background-color:#82d682 ;">
     <div class="container">
-        <a class="navbar-brand" href="#">Logo</a>
+        <a class="navbar-brand" href="#">
+            <img src="{{asset('images/logo.png')}}" alt="Logo" width="64" height="64">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
@@ -10,7 +12,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{url('home')}}">Home</a>
+                    <a class="nav-link activeø" aria-current="page" href="{{url('home')}}">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{url('about-us')}}">About Us</a>
